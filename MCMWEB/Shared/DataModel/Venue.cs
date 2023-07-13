70b925d1cee255c14c40ac09cb770f2f9dff6f27
HardikMasalawala88 <HardikMasalawala88@gmail.com>
2023-07-13 15:55:00 +0530

    Small changes

--- MCMWEB/Shared/DataModel/Venue.cs
+++ MCMWEB/Shared/DataModel/Venue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace MCMWEB.Shared.DataModel
     {
         public Guid id { get; set; }
         public string name { get; set; }
+
+        [MaxLength(10)]
         public string phone { get; set; }
         public string url { get; set; }
         public string address { get; set; }
