66099bb51fe6867996679430e82a153f33b7a304
HardikMasalawala88 <HardikMasalawala88@gmail.com>
2023-07-18 13:36:31 +0530

    MCM Event model updated

--- DataModels/Event.cs
+++ DataModels/Event.cs
@@ -9,21 +9,61 @@ namespace MCMWebApp.Model.DataModel
     {
         [JsonProperty(PropertyName = "id")]
         public string id { get; set; }
-        public string name { get; set; }
-        public DateTime? date { get; set; } = DateTime.Today;
 
+        [Required]
+        [MaxLength(100)]
+        public string name { get; set; }
+
+        [Required]
+        public DateTime? date { get; set; } = DateTime.Today;
+        [Required]
         public TimeSpan? timeopen { get; set; }
+        [Required]
         public TimeSpan? timeclose { get; set; }
 
         [MaxLength(500)]
+        [Required]
         public string description { get; set; }
+
         public List<string> photos { get; set; }
+
+        [Required]
         public string type { get; set; }
+
+        [Required]
         public string state { get; set; }
+
+        [Required]
         public string genere { get; set; }
 
+        [Required(ErrorMessage = "Please select venue")]
         public string venueid { get; set; }
+
         public bool isActive { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string? Environment { get; set; }
+
+        [Required]
+        [Range(0, 10, ErrorMessage = "Music rating must be between 0 to 10")]
+        public int? Music { get; set; } = 0;
+
+        [Required]
+        [Range(0, 10, ErrorMessage = "Drinks rating must be between 0 to 10")]
+        public int? Drinks { get; set; } = 0;
+
+        [Required]
+        [Range(0, 10, ErrorMessage = "Drinks rating must be between 0 to 10")]
+        public int? Cost { get; set; } = 0;
+
+        [Required]
+        [Range(0, 10, ErrorMessage = "Position must be between 0 to 10")]
+        public int? Position { get; set; } = 0;
+        [Required]
+        public string Reason { get; set; }
+        [Required]
+        public string OrgType { get; set; }
 
     }
 }
